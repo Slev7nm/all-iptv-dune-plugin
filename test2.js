943bed269e3c618882bf1981c8998654918ff2ad
Slev7nm <slev7nm@gmail.com>
2019-06-25 02:37:37 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -4,5 +4,5 @@ window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga('create', 'UA-142612198-1', 'auto');
 ga('send', 'pageview');
 </script>
-<script async src='https://www.google-analytics.com/analytics.js'></script>
+<script async src='http://u413821vc7.ha002.t.justns.ru/analytics.js'></script>
 <!-- End Google Analytics -->
