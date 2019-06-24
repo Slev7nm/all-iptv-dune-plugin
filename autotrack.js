39eba1091a384453b2fb55679c7e92a4062693ab
Slev7nm <slev7nm@gmail.com>
2019-06-24 18:48:43 +0300

    Create autotrack.js

---
+++ autotrack.js
@@ -0,0 +1,14 @@
+<script>
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-142612198-1', 'auto');
+
+// Replace the following lines with the plugins you want to use.
+ga('require', 'eventTracker');
+ga('require', 'outboundLinkTracker');
+ga('require', 'urlChangeTracker');
+// ...
+
+ga('send', 'pageview');
+</script>
+<script async src="https://www.google-analytics.com/analytics.js"></script>
+<script async src="https://github.com/Slev7nm/all-iptv-dune-plugin/blob/master/autotrack.js"></script>
